91b8bea3cadc88b55d0085572b22312cbf834636
macmanboy <macmanboy@foxmail.com>
2019-07-04 23:25:31 +0800

    finish the dml

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+INSERT table student values("","",18,"");
 -- 修改记录  
-
+update student set age = 20  where student.age<18
 -- 删除记录
-
--- 查询记录  
+delete * from student
+-- 查询记录
+selete * from student s where s.sex ='男';
